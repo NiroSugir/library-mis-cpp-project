a43b8e083c9e9bae47979989db443bfd5ea73c85
Niroshan Sugirtharatnam <codeniro@gmail.com>
2020-07-15 23:27:13 -0400

    text in textboxes are centered

--- LibraryManagementSystem/SignInPanel.h
+++ LibraryManagementSystem/SignInPanel.h
@@ -9,8 +9,8 @@ class SignInPanel : public wxPanel
 {
 private:
 	// screen elements
-	unique_ptr<wxTextCtrl> txt_username{ new wxTextCtrl{this, wxID_ANY, "", wxPoint{35, 144}, wxSize{248, 32} } };
-	unique_ptr<wxTextCtrl> txt_password{ new wxTextCtrl{this, wxID_ANY, "", wxPoint{35, 200}, wxSize{248, 32}, wxTE_PASSWORD} };
+	unique_ptr<wxTextCtrl> txt_username{ new wxTextCtrl{this, wxID_ANY, "", wxPoint{35, 144}, wxSize{248, 32}, wxTE_CENTER } };
+	unique_ptr<wxTextCtrl> txt_password{ new wxTextCtrl{this, wxID_ANY, "", wxPoint{35, 200}, wxSize{248, 32}, wxTE_CENTER | wxTE_PASSWORD} };
 	unique_ptr<wxButton> btn_enter{ new wxButton{this, wxID_ANY, "Enter", wxPoint{35, 256}, wxSize{120, 32} } };
 	unique_ptr<wxButton> btn_clear{ new wxButton{this, wxID_ANY, "Clear", wxPoint{164, 256}, wxSize{120, 32} } };
 
@@ -23,6 +23,8 @@ public:
 		// bind button click events
 		btn_enter->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &SignInPanel::onEnterClicked, this, wxID_ANY);
 		btn_clear->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &SignInPanel::onClearClicked, this, wxID_ANY);
+
+		txt_username->SetFocus();
 	}
 
 	void onEnterClicked(wxCommandEvent& evt)
